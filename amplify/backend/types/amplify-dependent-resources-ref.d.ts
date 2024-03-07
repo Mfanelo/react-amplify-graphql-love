a117a460cd92dec8812e3faf97288392390978f4
Mbele <mfanelom@909c4ace95e8.ant.amazon.com>
2024-03-07 23:02:54 +0200

    graphql and api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reactamplifygraphqll": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "reactamplifygraphqll0e6f8ec0": {
       "AppClientID": "string",
