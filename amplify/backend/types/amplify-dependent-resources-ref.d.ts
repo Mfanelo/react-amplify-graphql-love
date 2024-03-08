9486aa1776e66a3ceff139aaf4a6b4a7d68e7030
Mbele <mfanelom@909c4ace95e8.ant.amazon.com>
2024-03-08 03:01:42 +0200

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "imgstore": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
